c3eaa199ae1cdb62cae73b97ac848f2c0c15a343
aman-29-singh <singhaman41004@gmail.com>
2025-01-20 01:33:40 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer } from 'redux-persist';//we have to install this redux-persist from client terminal
+//import persistReducer from 'redux-persist/es/persistReducer';
+//import { version } from 'mongoose';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({user: userReducer})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+} /*isse agar page refresh bhi hogya toh bhi user ka information inspect karke application k andar localhost mein
+dekh sakte hai*/
+//Redux locally and globally bhi information ko store karta hai
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer, //we added this 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
-})
+});
+
+export const persistor = persistStore(store);
