9d2350fdcbac10a3d869e4e6a4bf546e39294c6d
aman-29-singh <singhaman41004@gmail.com>
2025-07-11 00:20:55 +0530

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";//import from model and .js at end
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async(req, res, next) => {
    /*here this async means asynchronous because we have to wait for response of mongodb and we gonna use 
@@ -10,4 +11,29 @@ export const createListing = async(req, res, next) => {
         next(error);
     }
 
+}
+
+export const deleteListing = async (req, res, next) => {
+    //first we will check if listing is exist or not
+    const listing = await Listing.findById(req.params.id);//find this listing id in database
+    
+    //if listing is not exist
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found'))
+    }
+
+    //if listing exist then we wanna check if the user is the owner of listing
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(401,'you can only delete your own listing!'));
+    }
+
+    //but if everything is ok then we will use try and catch
+    //and Inside the try we just want to delete the user
+    try{
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Listing has been deleted');//aise server Response bhejega json mein
+
+    } catch (error) {
+        next(error);
+    }
 }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing } from '../controllers/listing.controller.js';//yeh .js extension use karna mandotory hai
+import { createListing, deleteListing } from '../controllers/listing.controller.js';//yeh .js extension use karna mandotory hai
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create',verifyToken, createListing)//we wanna check if the person is authenticated or not
 //so that the person is not able to crete any listing if person is not authenticated so we have
 //we have to verify the user and then create the listing
+router.delete('/delete/:id', verifyToken, deleteListing)
 
 export default router;
